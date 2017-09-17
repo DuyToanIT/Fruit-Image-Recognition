8022cf767f0072b4e5130730cf78b01df3646390
thanhtuit96 <thanhtuit96@gmail.com>
2017-09-17 14:31:02 +0700

    first commit

---
+++ initData.py
@@ -0,0 +1,10 @@
+from PreProcessing.ScaleImage  import ScaleImage
+import os
+import os.path
+
+PATH='Data-Raw/'
+Labels = os.listdir(PATH)
+
+print "Sub: " , subfolders
+##im = ScaleImage('Data-Raw/Ambarella/')
+##files=im.readFile()
